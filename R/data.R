128680beb1b6c6c2fa8f553becdcbbf22f0256a7
rysz.k.lisiecki <r.k.lisiecki@code.hub.pl>
2023-12-20 07:19:45 +0100

    Datasets inclusion
    Including web-scraped datasets and their documentation: company_database, company_timesries and forex_timeseries. Man files generated with roxygen2.

---
+++ R/data.R
@@ -0,0 +1,64 @@
+#' Company overview of 2104 European and American stocks (from Yahoo Finance)
+#'
+#' A subset of Yahoo Finance profile and statistics data of 1161 US and
+#' 944 European companies (BE, DE, ES, FR, IT, NL, NO, PT)
+#' plus metadata about their stock timeseries in this library
+#' plus their USD and EUR market cap values after conversion
+#' at rate dated at last observation.
+#'
+#' @format ## `company_database`
+#' A data frame with 2104 rows and 13 columns:
+#' \describe{
+#'   \item{Name}{Company name on one of the Euronext exchanges or Polish brokerage houses}
+#'   \item{Ticker}{Company stock ticker used to collect data from Yahoo Finance}
+#'   \item{Country}{Country name defined with the ISIN of the security}
+#'   \item{Sector}{Company sector of operation defined by Yahoo Finance}
+#'   \item{Industry}{Company industry of operation defined by Yahoo Finance}
+#'   \item{Currency}{Stock quote currency of the particular ticker}
+#'   \item{NoOfEmployees}{Number of company employees in December 2023}
+#'   \item{MarketCap}{Company market capitalization in quote currency units}
+#'   \item{MarketCapUpdate}{Day of the market capitalization sum adequacy}
+#'   \item{TimeSeriesStart}{Beginning date of timeseries in the corresponding `company_timesries` dataset}
+#'   \item{TimeSeriesEnd}{End date of timeseries in the corresponding `company_timesries` dataset}
+#'   \item{EURMarketCap}{Market capitalization in Euros}
+#'   \item{USDMarketCap}{Market capitalization in US Dollars}
+#' }
+#' @source <https://finance.yahoo.com/quote/>
+"company_database"
+
+#' Historical stock prices - monthly closes of 2104 stocks (from Yahoo Finance)
+#'
+#' A subset of Yahoo Finance timeseries history for 2104 stock
+#' monthly close prices adjusted for splits and not for dividends.
+#' Covering the timespan from January 2018 to December 2023.
+#' Column names of this dataset correspond to Yahoo Finance tickers
+#' and the `Ticker`column in the `company_database` dataset.
+#'
+#' @format ## `company_timesries`
+#' An xts object on 2018-01-31 / 2023/11/30 containing:
+#' Data: double [71, 2104]
+#' Columns: ABCA.PA, ALCRB.PA, ORIA.PA ... and 2101 more columns (Tickers used in Yahoo Finance)
+#' Index: Date [71] (TZ: "UTC")
+#'
+#' @source <https://finance.yahoo.com/quote/.../history>
+"company_timeseries"
+
+#' Historical FOREX bid prices - monthly close of EURUSD and EURNOK (from Yahoo Finance)
+#'
+#' A subset of Yahoo Finance timeseries history for EURUSD (`EURUSD=X`)
+#' and EURNOK (`EURNOK=X`) monthly close bid prices.
+#' Covering the timespan from January 2018 to December 2023.
+#' Columns names of this dataset DO NOT correspond to Yahoo Finance tickers
+#' and ARE NOT available in the `company_database` dataset.
+#'
+#' @format ## `forex_timeseries`
+#' An xts object on 2018-01-31 / 2023/11/30 containing:
+#' Data: double [71, 2]
+#' Columns: EURUSD, EURNOK
+#' Index:   Date [71] (TZ: "UTC")
+#' \describe{
+#'   \item{EURUSD}{EUR/USD bid FX spot rate}
+#'   \item{EURNOK}{EUR/NOK bid FX spot rate}
+#' }
+#' @source <https://finance.yahoo.com/quote/.../history>
+"forex_timeseries"
